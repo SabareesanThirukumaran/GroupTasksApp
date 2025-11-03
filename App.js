720c6b16591f3e13659700f5864438c3a456f859
Sabareesan Thirukumaran <thirukumaransabareesan18@gmail.com>
2025-11-03 16:16:07 +0000

    Finished final changes

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Slot} from "expo-router";
+import * as SystemUI from 'expo-system-ui';
+SystemUI.setBackgroundColorAsync('transparent');
 
 export default function App() {
   return (
+    
     <GestureHandlerRootView style={styles.container}>
       <Slot />
       <StatusBar style="auto" />
